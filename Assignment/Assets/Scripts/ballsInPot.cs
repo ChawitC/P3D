5d626bef06a6e2a75f2ff2823f8c1842db6d56d7
ChawitC <91472453+ChawitC@users.noreply.github.com>
2021-11-29 18:57:49 +0000

    Wk 11 a
    VFX Graph with variable intensity based on number of balls in the pot

---
+++ Assignment/Assets/Scripts/ballsInPot.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.VFX;
+
+public class ballsInPot : MonoBehaviour
+{
+	int objects = 0;
+	[SerializeField] private string ballTag;
+	[SerializeField] private Text ballcountText;
+	[SerializeField] private objectivesUpdater objective; //Exposing objectiveUpdater script
+	[SerializeField] private Light potLight;
+	//[SerializeField] private float smokeRate;
+	//[SerializeField] private float spawnRate;
+	//[SerializeField] private AudioClip[] fireSounds;
+	AudioSource audioSource;
+	VisualEffect visualEffect;
+	private float smokeRateCalc;
+	private float splinterRateCalc;
+	
+    // Update is called once per frame
+	void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+		visualEffect = GetComponent<VisualEffect>();	
+    }
+	
+    void Update()
+    {
+        //Debug.Log("Balls in pot: " + objects);
+		
+		potLight.intensity = 5000000f + (objects*3000000f); //increase intensity based on number of balls in the pot
+		smokeRateCalc = 1f + (objects*.2f); // Calculate rate of smoke effect spawning based on number of balls in the pot
+		splinterRateCalc = 10f + (objects*20f); // Calculate rate of splinter effect spawning based on number of balls in the pot
+		visualEffect.SetFloat("smokeRate",smokeRateCalc);
+		visualEffect.SetFloat("splinterRate",splinterRateCalc);
+		ballcountText.text = objects.ToString("#");
+    }
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == ballTag)
+		{
+		PlayFireAudio();
+		objects++;
+		//objectivesUpdater.obj3 = true; //Updating objective, since player has dropped the ball into the pot
+		objective.Obj3done = true;
+		}
+	}
+
+
+	private void OnTriggerExit(Collider other)
+	{
+		if(other.tag == ballTag)
+		{
+		objects--;
+		}
+	}
+	private void PlayFireAudio()
+	{	
+			//audioSource.clip = fireSounds[0];
+			audioSource.PlayOneShot(audioSource.clip);
+	}
+}
